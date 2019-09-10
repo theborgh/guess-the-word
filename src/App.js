b033f556d8f774013a0dc9661bf7f0ad00abbdaa
theborgh <borghiborghi@gmail.com>
2019-09-10 10:20:36 +0200

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+      <h1>Guess the word</h1>
+      <Congrats success={true} />
+      <GuessedWords guessedWords={[
+        { guessedWord: 'bobsz', letterMatchCount: 3 },
+        { guessedWord: 'bubsz', letterMatchCount: 4 },
+        { guessedWord: 'byubs', letterMatchCount: 2 },
+      ]} />
     </div>
   );
 }
--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -6,7 +6,6 @@ const GuessedWords = (props) => {
   if (props.guessedWords.length === 0) {
     contents = <span data-test='guess-instructions'>Type your guess!</span>;
   } else {
-    console.log(props.guessedWords)
     const guessedWordsRows = props.guessedWords.map(
       (word, index) => (
         <tr data-test="guessed-word" key={index} >
@@ -18,8 +17,8 @@ const GuessedWords = (props) => {
     contents = (
       <div data-test="guessed-words">
         <h3>Guessed words</h3>
-        <table>
-          <thead>
+        <table className="table table-sm">
+          <thead className="thead-light">
             <tr>
               <th>Guess</th><th>Matching letters</th>
             </tr>
