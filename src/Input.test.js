2062985b3be98a10c629da8781d2debb8093683f
theborgh <borghiborghi@gmail.com>
2019-09-11 20:05:49 +0200

    Add redux-thunk

--- src/Input.test.js
+++ src/Input.test.js
@@ -38,11 +38,25 @@ describe("Render", () => {
   });
 
   describe("Word has been guessed", () => {
-    it("renders without errors", () => {});
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup({success: true});
+    });
+    it("renders without errors", () => {
+      expect(findTagsWithTestAttribute(wrapper, "component-input").length).toBe(
+        1
+      );
+    });
 
-    it("does not render the input box", () => {});
+    it("does not render the input box", () => {
+      expect(findTagsWithTestAttribute(wrapper, "input-box").length).toBe(0);
+    });
 
-    it("does not render the submit button", () => {});
+    it("does not render the submit button", () => {
+      expect(findTagsWithTestAttribute(wrapper, "submit-button").length).toBe(
+        0
+      );
+    });
   });
 });
 
